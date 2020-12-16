4b8cbfbf28e1ed0e77e5a54e6d7d26ad6f89dd75
sydneygold <sydney.goldberg@outlook.com>
2020-12-16 12:53:07 -0700

    Added contact info

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App() {
       <section className="header">
         <h1 className="title">Sydney Goldberg</h1>
         <h2 className="coming-soon">Coming Soon...</h2>
+        <p className="info">In the meantime, checkout my Github and LinkedIn!</p>
+        <p className="info">Email: sydney.goldberg@outlook.com</p>
+        <a className="info" href="https://github.com/sydneygold">Github</a>
+        <a className="info" href="https://www.linkedin.com/in/sydney-goldberg-32b9751b0/">LinkedIn</a>
       </section>
     </main>
   );
